a0e1f4248b5de115a2af1803c761b81d29912272
WaggishYogesh <97587863+WaggishYogesh@users.noreply.github.com>
2022-01-14 19:03:45 +0530

    Add files via upload

---
+++ output1.py
@@ -0,0 +1,29 @@
+1st program
+Enter first number: 10
+Enter second number: 10
+Enter third number: 10
+Average of numbers is:  10.0
+2nd program
+
+Enter your gross income : 200000
+Enter number of dependents : 6
+Tax to be paid is $ 34400.0 
+
+3rd program
+Enter the sid = 21104122
+Enter the name = Yogesh
+Enter the gender F/M = M
+Enter the branch name = EE
+Enter the cgpa = 10.0
+Student:  [21104122, 'Yogesh', 'M', 'EE', 10.0]
+4th program
+Enter the marks of 1st student= 87
+Enter the marks of 2nd student= 89
+Enter the marks of 3rd student= 87
+Enter the marks of 4th student= 54
+Enter the marks of 5th student= 65
+Marks =  [54.0, 65.0, 87.0, 87.0, 89.0]
+5th program
+ORIGINAL LIST: ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+List after removing 4th color:  ['Red', 'Green', 'White', 'Pink', 'Yellow']
+List after replacing black and pink by purple:  ['Red', 'Green', 'White', 'Purple', 'Yellow']
