aed1506f3c906b2c17a50e4ef52ba34c2c0a9ff7
WaggishYogesh <97587863+WaggishYogesh@users.noreply.github.com>
2022-01-26 14:54:22 +0530

    Add files via upload

---
+++ output2.py
@@ -0,0 +1,49 @@
+Question1
+
+ (a)
+Length of Input String = 35
+
+ (b)
+egaugnal evitisnes esac a si nohtyP
+
+ (c)
+a case sensitive
+
+ (d)
+Python is object oriented language
+
+ (e)
+10
+
+ (f)
+Pythonisacasesensitivelanguage
+
+Question 2
+
+Hey, Yogesh  here! 
+My SID is 21104122 
+I am from EE and my CGPA is 9.9
+
+Question 3
+ a&b is :  8
+ a|b is :  58
+ a^b  is :  50
+a<<2 :  224 	b<<2 : 40
+a>>2 :  14 	b>>2 : 0
+
+Question 4
+Enter 1st  : 10
+Enter 2nd  : 20
+Enter 3rd  : 30
+The Highest number is = 30
+
+Qustion 5
+Enter input string : dfgsdf
+No
+
+Question 6
+
+Enter l1  : 1
+Enter l2  : 2
+Enter l3  : 3
+No
